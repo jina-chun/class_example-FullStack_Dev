fb44af6c533a07d8d03b711f6d5f3382ac769774
Jina <yongjinc@bu.edu>
2020-10-24 05:51:47 -0400

    ps4 started

--- PS3/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CS412 PS3 Jina Chun U15518893' });
-});
-
-module.exports = router;
